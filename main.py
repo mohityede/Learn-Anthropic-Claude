4221df2de234259e9edf8eb1277b5cdd9343d3e8
mohityede <mohityede755@gmail.com>
2026-07-29 18:15:15 +0530

    created test_llm_call funtion for testing

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 from anthropic import Anthropic
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 client = Anthropic(
-    api_key="sk-1234",
+    api_key=os.getenv("LITELLM_MASTER_KEY"),
     base_url="http://localhost:4000"
 )
 
-def main():
-    print("Hello from learn-anthropic-claude!")
+def test_llm_call(message):
     response = client.messages.create(
-        model="local",
+        model="gemini",
         max_tokens=1000,
         messages=[
             {
                 "role": "user",
-                "content": "Explain vector databases in one sentence"
+                "content": message
             }
         ]
     )
 
     print(response.content[0].text)
+
+def main():
+    print("Hello from learn-anthropic-claude!")
+    test_llm_call("Explain vector databases in one sentence")
+    
 
 
 if __name__ == "__main__":
